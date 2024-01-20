62ab1f9ceb069946c4531be0310e6c419cb7fa82
Prudencerich <maryejiropraise@gmail.com>
2024-01-20 17:16:24 +0100

    web hosting completion cont

--- myfirstproject/myfirstproject/settings.py
+++ myfirstproject/myfirstproject/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -81,13 +83,24 @@ WSGI_APPLICATION = 'myfirstproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+  'default': {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': 'neondb',
+    'USER': 'maryejiropraise',
+    'PASSWORD': 'O0mZLSQK4vID',
+    'HOST': 'ep-fancy-leaf-a5jmlvzs-pooler.us-east-2.aws.neon.tech',
+    'PORT': '5432',
+    'OPTIONS': {'sslmode': 'require'},
+    'DISABLE_SERVER_SIDE_CURSORS': True,
+  }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators

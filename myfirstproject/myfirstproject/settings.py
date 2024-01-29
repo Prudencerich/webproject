540607bd8e7f1f245633bf4b94ec802cf2e0a960
Prudencerich <maryejiropraise@gmail.com>
2024-01-29 23:11:08 +0100

    web hosting completion cont

--- myfirstproject/myfirstproject/settings.py
+++ myfirstproject/myfirstproject/settings.py
@@ -55,7 +55,6 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'responsive.middleware.ResponsiveMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -72,8 +71,6 @@ TEMPLATES = [
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
-                'django.core.context_processors.request',
-                'responsive.context_processors.device',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
